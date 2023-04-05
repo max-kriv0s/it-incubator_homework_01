bf93f8490d739f36942411fd647a5ee2dbf5a540
Maxim Kr <max.kriv0s@gmail.com>
2023-04-05 21:33:14 +0300

    add URIParams

--- src/types.ts/types.ts
+++ src/types.ts/types.ts
@@ -1,4 +1,5 @@
 import {Request} from "express"
 
+export type URIParams<T> = Request<T>
 export type RequestsWithBody<T> = Request<{},{},T>
 export type RequestsWithParamsAndBody<P, B> = Request<P,{},B>
