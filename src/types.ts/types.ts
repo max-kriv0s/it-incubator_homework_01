9dba5e1ea8cd8813175ea0411622e17d5ad2cc80
Maxim Kr <max.kriv0s@gmail.com>
2023-04-05 21:34:29 +0300

    change URIParams to RequestsURIParams

--- src/types.ts/types.ts
+++ src/types.ts/types.ts
@@ -1,5 +1,5 @@
 import {Request} from "express"
 
-export type URIParams<T> = Request<T>
+export type RequestsURIParams<T> = Request<T>
 export type RequestsWithBody<T> = Request<{},{},T>
 export type RequestsWithParamsAndBody<P, B> = Request<P,{},B>
