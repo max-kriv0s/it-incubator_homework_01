bfc8576774c73a7b65191f9f850a6f499c6f67bc
Maxim Kr <max.kriv0s@gmail.com>
2023-04-05 22:56:26 +0300

    Post-validation-middleware

---
+++ src/middlewares/Post-validation-middleware.ts
@@ -0,0 +1,30 @@
+import { body } from "express-validator";
+import { blogsRepository } from "../repositories/blogs-repository";
+
+export const PostValidate = [
+    body('title')
+        .exists({ checkFalsy: true }).bail()
+        .isString().bail()
+        .isLength({ max: 30 })
+        .withMessage('must be no more than 30 chars'),    
+    body('shortDescription')
+        .exists({ checkFalsy: true }).bail()
+        .isString().bail()
+        .isLength({ max: 100 })
+        .withMessage('must be no more than 100 chars'),  
+    body('content')
+        .exists({ checkFalsy: true }).bail()
+        .isString().bail()
+        .isLength({ max: 1000 })
+        .withMessage('must be no more than 1000 chars'),
+    body('blogId')
+        .exists({ checkFalsy: true }).bail()
+        .isString().bail()
+        .custom(value => {
+            const blog = blogsRepository.getBlog(value)
+            if (!blog) {
+                throw new Error('Blog not found')
+            }
+            return true
+        })               
+]
