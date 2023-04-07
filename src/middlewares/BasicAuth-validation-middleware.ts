b0ed5b9654317e4a2cfbdcc2aeb7094e2760e224
Maxim Kr <max.kriv0s@gmail.com>
2023-04-08 01:10:05 +0300

    changed BasicAuth-validation-middleware

--- src/middlewares/BasicAuth-validation-middleware.ts
+++ src/middlewares/BasicAuth-validation-middleware.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express"
 import { StatusCodes } from "http-status-codes"
 
 
-const admin = 'Basic YWRtaW46cXdlcnR5'
+const admin = process.env.ADMIN_LOGIN
 
 export const BasicAuthValidate = (req: Request, res: Response, next: NextFunction) => {
     if (req.headers.authorization && req.headers.authorization === admin) {
