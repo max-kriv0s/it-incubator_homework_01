ff19d9a49415f4f837f318aeb1c60116bf4537ec
Maxim Kr <max.kriv0s@gmail.com>
2023-04-08 01:36:18 +0300

    added trim() to blog and post validation

--- src/middlewares/Blog-validation-middleware.ts
+++ src/middlewares/Blog-validation-middleware.ts
@@ -4,16 +4,19 @@ const reWebsiteUrl = new RegExp('^https://([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a
 
 export const BlogValidate = [
     body('name')
+        .trim()
         .exists({ checkFalsy: true }).bail()
         .isString().bail()
         .isLength({ max: 15 })
         .withMessage('must be no more than 15 chars'),
     body('description')
+        .trim()    
         .exists({ checkFalsy: true }).bail()
         .isString().bail()
         .isLength({ max: 500 })
         .withMessage('must be no more than 500 chars'),
     body('websiteUrl')
+        .trim()
         .exists({ checkFalsy: true }).bail()
         .isString().bail()
         .isLength({ max: 100 })
--- src/middlewares/Post-validation-middleware.ts
+++ src/middlewares/Post-validation-middleware.ts
@@ -3,21 +3,25 @@ import { blogsRepository } from "../repositories/blogs-repository";
 
 export const PostValidate = [
     body('title')
+        .trim()
         .exists({ checkFalsy: true }).bail()
         .isString().bail()
         .isLength({ max: 30 })
         .withMessage('must be no more than 30 chars'),    
     body('shortDescription')
+        .trim()
         .exists({ checkFalsy: true }).bail()
         .isString().bail()
         .isLength({ max: 100 })
         .withMessage('must be no more than 100 chars'),  
     body('content')
+        .trim()
         .exists({ checkFalsy: true }).bail()
         .isString().bail()
         .isLength({ max: 1000 })
         .withMessage('must be no more than 1000 chars'),
     body('blogId')
+        .trim()    
         .exists({ checkFalsy: true }).bail()
         .isString().bail()
         .custom(value => {
