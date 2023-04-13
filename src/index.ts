6d01486ec283f964616fd2bf24aca5c4f1b9ddcd
Maxim Kr <max.kriv0s@gmail.com>
2023-04-13 08:53:56 +0300

    changed to App async

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-import 'dotenv/config'
+import dotenv from 'dotenv'
+dotenv.config()
+
 import { app } from "./setting"
+import { runDB } from './repositories/db'
 
 const port = process.env.PORT || 5000
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const startApp = async () => {
+    await runDB()
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`)
+    })
+}
+
+startApp()
