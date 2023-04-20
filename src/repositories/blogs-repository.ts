14797c002a22ad1f81c8745e354f2c03edb20aec
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-04-20 07:31:55 +0300

    fix blogs

--- src/repositories/blogs-repository.ts
+++ src/repositories/blogs-repository.ts
@@ -1,38 +1,53 @@
-import { BlogCreateModel } from "../models/blogs/BlogCreateModel"
-import { BlogViewModel } from "../models/blogs/BlogViewModel"
 import { BlogUpdateModel } from "../models/blogs/BlogUpdateModel"
-import { newStringId } from "../utils/utils"
 import { blogsCollection } from "./db"
+import { PaginatorBlogDbTypes } from "../types.ts/PaginatorType"
+import { BlogDbModel } from "../models/blogs/BlogDbModel"
+import { ObjectId } from "mongodb"
 
 
 export const blogsRepository = {
-    async getBlogs(): Promise<BlogViewModel[]> {
-        return blogsCollection.find({}, {projection: {'_id': 0}}).toArray()
+    async getBlogs(searchNameTerm: string | null, 
+        pageNumber: number, 
+        pageSize: number,
+        sortBy: string,
+        sortDirection: string): Promise<PaginatorBlogDbTypes> {
+            
+            const filter: any = {}
+            if (searchNameTerm) {
+                filter.name = { $regex: searchNameTerm }
+            }
+            
+            const totalCount: number = await blogsCollection.countDocuments(filter)
+            
+            const skip = (pageNumber - 1) * pageSize
+            const blogs: BlogDbModel[] = await blogsCollection.find(filter)
+                .sort({ [sortBy]: sortDirection === 'asc' ? 1 : -1 })
+                .skip(skip)
+                .limit(pageSize).toArray()
+
+            return {
+                pagesCount: Math.ceil(totalCount / pageSize),
+                page: pageNumber,
+                pageSize: pageSize,
+                totalCount: totalCount,
+                items: blogs
+            }
     },
 
-    async findBlogById(id: string): Promise<BlogViewModel | null> {
-        const blog:BlogViewModel | null = await blogsCollection.findOne({ id: id}, {projection: {'_id': 0}})
+    async findBlogById(id: string): Promise<BlogDbModel | null> {
+        const blog:BlogDbModel | null = await blogsCollection.findOne({ _id: new ObjectId(id)})
         return blog
     },
 
-    async createBlog(body: BlogCreateModel): Promise<BlogViewModel> {
-        const newBlog = {
-            id: newStringId(),
-            name: body.name,
-            description: body.description,
-            websiteUrl: body.websiteUrl,
-            isMembership: false,
-            createdAt: new Date().toISOString()
-        }
-
-        const result = await blogsCollection.insertOne({...newBlog})
+    async createBlog(newBlog: BlogDbModel): Promise<BlogDbModel> {
+        const result = await blogsCollection.insertOne(newBlog)
         return newBlog      
         
     },
 
     async updateBlog(id: string, body: BlogUpdateModel): Promise<boolean> {
         const result = await blogsCollection.updateOne(
-            { id: id},
+            { _id: new ObjectId(id) },
             { $set: {
                 name: body.name,
                 description: body.description,
@@ -43,10 +58,11 @@ export const blogsRepository = {
         return result.matchedCount === 1
     },
 
-    async deleteBlog(id: string): Promise<boolean> {
-        const result = await blogsCollection.deleteOne({ id: id})
+    async deleteBlogById(id: string): Promise<boolean> {
+        const result = await blogsCollection.deleteOne({ _id: new ObjectId(id) })
         return result.deletedCount === 1
     }, 
+
     async deleteBlogs() {
         blogsCollection.deleteMany({})
     }
--- src/routes/blogs-router.ts
+++ src/routes/blogs-router.ts
@@ -1,21 +1,22 @@
 import { Router, Request, Response } from "express"
-
-import { blogsRepository } from "../repositories/blogs-repository"
 import { BlogViewModel } from "../models/blogs/BlogViewModel"
 import { BlogCreateModel } from "../models/blogs/BlogCreateModel"
-import { RequestsWithBody, RequestsWithParamsAndBody } from "../types.ts/types"
+import { RequestsQuery, RequestsWithBody, RequestsWithParamsAndBody, RequestsWithParamsAndQuery } from "../types.ts/types"
 import { StatusCodes } from "http-status-codes"
 import { BasicAuthValidate } from "../middlewares/BasicAuth-validation-middleware"
 import { ErrorsValidate } from "../middlewares/Errors-middleware"
 import { BlogValidate } from "../middlewares/Blog-validation-middleware"
 import { BlogUpdateModel } from "../models/blogs/BlogUpdateModel"
 import { URIParamsIdModel } from "../types.ts/URIParamsIdModel"
+import { PaginatorBlogViewTypes, PaginatorTypes } from "../types.ts/PaginatorType"
+import { QueryParamsModels } from "../types.ts/QueryParamsModels"
+import { blogsServise } from "../domain/blogs-service"
 
 
 export const routerBlogs = Router()
 
-routerBlogs.get('/', async (req: Request, res: Response<BlogViewModel[]>) => {
-    const blogs: BlogViewModel[] = await blogsRepository.getBlogs()
+routerBlogs.get('/', async (req: RequestsQuery<QueryParamsModels>, res: Response<PaginatorBlogViewTypes>) => {
+    const blogs: PaginatorBlogViewTypes = await blogsServise.getBlogs(req.query)
     res.send(blogs)
 })
 
@@ -24,12 +25,12 @@ routerBlogs.post('/',
     BlogValidate,
     ErrorsValidate,
     async (req: RequestsWithBody<BlogCreateModel>, res: Response<BlogViewModel>) => {
-        const newBlog = await blogsRepository.createBlog(req.body)
+        const newBlog = await blogsServise.createBlog(req.body)
         res.status(StatusCodes.CREATED).send(newBlog)
 })
 
 routerBlogs.get('/:id', async (req: Request<URIParamsIdModel>, res: Response<BlogViewModel>) => {
-    const blog = await blogsRepository.findBlogById(req.params.id)
+    const blog = await blogsServise.findBlogById(req.params.id)
     if (blog) {
         res.send(blog)
     } else {
@@ -42,7 +43,7 @@ routerBlogs.put('/:id',
     BlogValidate,
     ErrorsValidate,
     async (req: RequestsWithParamsAndBody<URIParamsIdModel, BlogUpdateModel>, res: Response) => {
-        const isUpdate = await blogsRepository.updateBlog(req.params.id, req.body)
+        const isUpdate = await blogsServise.updateBlog(req.params.id, req.body)
         if (isUpdate) {
             res.sendStatus(StatusCodes.NO_CONTENT)    
         } else {
@@ -53,10 +54,18 @@ routerBlogs.put('/:id',
 routerBlogs.delete('/:id', 
     BasicAuthValidate,
     async (req: Request<URIParamsIdModel>, res: Response) => {
-        const isDelete = await blogsRepository.deleteBlog(req.params.id)
+        const isDelete = await blogsServise.deleteBlogById(req.params.id)
         if (isDelete) {            
             res.sendStatus(StatusCodes.NO_CONTENT)      
         } else {
             res.sendStatus(StatusCodes.NOT_FOUND)
         }   
 })
+
+// routerBlogs.get('/:id/posts', (req: RequestsWithParamsAndQuery<URIParamsIdModel, QueryParamsModels>
+//     , res) => {
+//         const pageNumber:number = +req.query.pageNumber ?? 1
+//         const pageSize: number = +req.query.pageSize ?? 10
+//         const sortBy:string = req.query.sortBy ?? 'createdAt'
+//         const sortDirection = req.query.sortDirection ?? 'desc'
+// })
