1fe3952340a35eae8c488f0092836ca2297086b3
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-04-24 16:44:29 +0300

    added PaginatorUser

--- src/types.ts/PaginatorType.ts
+++ src/types.ts/PaginatorType.ts
@@ -2,6 +2,8 @@ import { BlogDbModel } from "../models/blogs/BlogDbModel"
 import { BlogViewModel } from "../models/blogs/BlogViewModel"
 import { PostDbModel } from "../models/posts/PostDbModel"
 import { PostViewModel } from "../models/posts/PostViewModel"
+import { UserDBModel } from "../models/users/UserDBModel"
+import { UserViewModel } from "../models/users/UserViewModel"
 
 export type PaginatorTypes<T> = {
     pagesCount: number
@@ -13,5 +15,9 @@ export type PaginatorTypes<T> = {
 
 export type PaginatorBlogViewTypes = PaginatorTypes<BlogViewModel>
 export type PaginatorBlogDbTypes = PaginatorTypes<BlogDbModel>
+
 export type PaginatorPostViewTypes = PaginatorTypes<PostViewModel>
 export type PaginatorPostDbTypes = PaginatorTypes<PostDbModel>
+
+export type PaginatorUserViewModel = PaginatorTypes<UserViewModel>
+export type PaginatorUserDBModel = PaginatorTypes<UserDBModel>
