87b3f5fcff93fd7ce3df617cdc471c5034043b41
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-04-24 16:42:25 +0300

    added Users-validation-middleware.ts

---
+++ src/middlewares/Users-validation-middleware.ts
@@ -0,0 +1,23 @@
+import { body } from "express-validator"
+
+
+export const UserValidate = [
+    body('login')
+        .trim()
+        .exists({ checkFalsy: true })
+        .isString()
+        .isLength({ min: 3, max: 10 })
+        .withMessage('must be between 3 and 15 characters')
+        .matches('^[a-zA-Z0-9_-]*$'),
+    body('password')
+        .trim()    
+        .exists({ checkFalsy: true })
+        .isString()
+        .isLength({ min: 6, max: 20 })
+        .withMessage('must be between 6 and 20 characters'),
+    body('email')
+        .trim()
+        .exists({ checkFalsy: true })
+        .isString()
+        .matches('^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$')
+]
