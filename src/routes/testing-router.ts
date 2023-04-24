7235f86019a663494ce5c5c840ee6bb7787b92fb
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-04-24 16:43:40 +0300

    added deleteUsers

--- src/routes/testing-router.ts
+++ src/routes/testing-router.ts
@@ -2,6 +2,7 @@ import { Router, Request, Response } from "express";
 import { videoRepository } from "../repositories/videos-repository";
 import { blogsRepository } from "../repositories/blogs-repository";
 import { postsRepository } from "../repositories/posts-repository";
+import { usersRepository } from "../repositories/users-repository";
 
 
 export const routerTesting = Router()
@@ -10,6 +11,7 @@ routerTesting.delete('/all-data', async (req: Request, res: Response) => {
     videoRepository.deleteVideos()
     blogsRepository.deleteBlogs()
     postsRepository.deletePosts()
+    usersRepository.deleteUsers()
 
     res.send(204)
 })
