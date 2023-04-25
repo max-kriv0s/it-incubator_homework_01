22b28c9773bf373c0d164dc058d5f799af3b2e95
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-04-25 08:16:00 +0300

    added ErrorsValidate

--- src/routes/auth-router.ts
+++ src/routes/auth-router.ts
@@ -4,12 +4,14 @@ import { LoginInputModel } from "../models/auth/LoginInputModel";
 import { LoginValidation } from "../middlewares/Login-validation-middleware";
 import { StatusCodes } from "http-status-codes";
 import { usersService } from "../domain/users-service";
+import { ErrorsValidate } from "../middlewares/Errors-middleware";
 
 
 export const routerAuth = Router()
 
 routerAuth.post('/login', 
     LoginValidation,
+    ErrorsValidate,
     async (req: RequestsWithBody<LoginInputModel>, res: Response) => {
         const validPassword = await usersService.checkCredentials(req.body.loginOrEmail, req.body.password)
         if (validPassword) {
