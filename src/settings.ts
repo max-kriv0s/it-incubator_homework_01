4a73769510bcdc274f98342f3fe548443218e86e
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-04-28 20:44:14 +0300

    added settings.ts

---
+++ src/settings.ts
@@ -0,0 +1,6 @@
+export const settings = {
+    PORT: process.env.PORT || 5000,
+    MONGO_URI: process.env.MONGO_URI || "mongodb://127.0.0.1:27017/",
+    ADMIN_LOGIN: process.env.ADMIN_LOGIN,
+    JWT_SECRET: process.env.JWT_SECRET || Y3StVROKtxUI
+}
