9ce13c33141c01bd409ad26883c89c7d9739f89d
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-04-29 01:37:33 +0300

    added blogDBToBlogView, postDBToPostView, userDBToUserView

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,14 @@
+import { BlogDbModel } from "../models/blogs/BlogDbModel";
+import { BlogViewModel } from "../models/blogs/BlogViewModel";
+import { PostDbModel } from "../models/posts/PostDbModel";
+import { PostViewModel } from "../models/posts/PostViewModel";
+import { UserDBModel } from "../models/users/UserDBModel";
+import { UserViewModel } from "../models/users/UserViewModel";
 import { DataBaseModel } from "../types/DataBaseMode";
 
 export function randomString(n: number) {
     let rnd = '';
-    while (rnd.length < n) 
+    while (rnd.length < n)
         rnd += Math.random().toString(36).substring(2);
     return rnd.substring(0, n);
 }
@@ -18,15 +24,37 @@ export function newStringId() {
 }
 
 export function newNumberId() {
-    return +(new Date()) 
+    return +(new Date())
 }
 
-// export function deleteValueById(db: DataBaseModel , id: string| number): boolean {
-//     for (let i = 0; i < db.length; i++) {
-//         if (db[i].id === id) {
-//             db.splice(i, 1)
-//             return true
-//         }
-//     }
-//     return false
-// }
+export function blogDBToBlogView(blog: BlogDbModel): BlogViewModel {
+    return {
+        id: blog._id.toString(),
+        name: blog.name,
+        description: blog.description,
+        websiteUrl: blog.websiteUrl,
+        createdAt: blog.createdAt,
+        isMembership: blog.isMembership
+    }
+}
+
+export function postDBToPostView(post: PostDbModel): PostViewModel {
+    return {
+        id: post._id.toString(),
+        title: post.title,
+        shortDescription: post.shortDescription,
+        content: post.content,
+        blogId: post.blogId.toString(),
+        blogName: post.blogName,
+        createdAt: post.createdAt
+    }
+}
+
+export function userDBToUserView(user: UserDBModel): UserViewModel {
+    return {
+        id: user._id.toString(),
+        login: user.login,
+        email: user.email,
+        createdAt: user.createdAt
+    }
+}
