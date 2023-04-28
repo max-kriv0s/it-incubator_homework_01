4da16c34ae4c773f4cf41241727d71cb01cc1122
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-04-29 01:36:59 +0300

    fix jwt_secret

--- src/settings.ts
+++ src/settings.ts
@@ -2,5 +2,5 @@ export const settings = {
     PORT: process.env.PORT || 5000,
     MONGO_URI: process.env.MONGO_URI || "mongodb://127.0.0.1:27017/",
     ADMIN_LOGIN: process.env.ADMIN_LOGIN,
-    JWT_SECRET: process.env.JWT_SECRET || Y3StVROKtxUI
+    JWT_SECRET: process.env.JWT_SECRET || "Y3StVROKtxUI"
 }
