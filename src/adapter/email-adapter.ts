de8fc992fdd8670c1343bada68f747a6dd1203c2
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-05-07 16:30:39 +0300

    added email-adapter.ts

---
+++ src/adapter/email-adapter.ts
@@ -0,0 +1,34 @@
+import nodemailer from "nodemailer"
+import { settings } from "../settings";
+
+const TECH_EMAIL = settings.TECH_EMAIL
+const TECH_EMAIL_PASSWORD = settings.TECH_EMAIL_PASSWORD
+
+
+export const emailAdapter = {
+
+    async sendEmail(email: string, subject: string, textMessage: string) {
+
+        let transport = nodemailer.createTransport({
+            host: "smtp.yandex.ru",
+            port: 465,
+            secure: true,
+            // service: "gmail",
+            auth: {
+                user: TECH_EMAIL,
+                pass: TECH_EMAIL_PASSWORD,
+            },
+        });
+        
+        let message = {
+            from: `Learning platform <${TECH_EMAIL}>`,
+            to: email,
+            subject: subject,
+            html: textMessage
+        }
+
+        let info = await transport.sendMail(message);
+        
+        return info
+    }
+}
