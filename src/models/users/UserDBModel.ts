bc1c0e006dd196f942d52dedc8a6aa443f358499
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-05-07 16:36:14 +0300

    fix UserDBModel

--- src/models/users/UserDBModel.ts
+++ src/models/users/UserDBModel.ts
@@ -2,8 +2,19 @@ import { ObjectId } from "mongodb"
 
 export type UserDBModel = {
     _id: ObjectId
+    accountData: accountData
+    emailConfirmation: UserEmailConfirmationType
+}
+
+export type accountData = {
     login: string
     password: string
     email: string
-    createdAt: string
+    createdAt: string,
+}
+
+export type UserEmailConfirmationType = {
+    confirmationCode: string
+    expirationDate: Date
+    isConfirmed: boolean
 }
