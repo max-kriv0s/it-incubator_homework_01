8d8e2b740567d2cf2021aae7e9c16ce239d9af51
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-05-07 16:30:59 +0300

    added email-managers.ts

---
+++ src/managers/email-managers.ts
@@ -0,0 +1,29 @@
+import { emailAdapter } from "../adapter/email-adapter";
+import { UserDBModel } from "../models/users/UserDBModel";
+import { settings } from "../settings";
+
+const APP_URL = settings.APP_URL
+
+
+export const emailManager = {
+
+    async sendEmailConfirmationMessage(user: UserDBModel) {
+        const textMessage = `<h1>Thank for your registration</h1>
+        <p>To finish registration please follow the link below:
+            <a href='${APP_URL}/confirm-email?code=${user.emailConfirmation.confirmationCode}'>complete registration</a>
+        </p>`
+        
+        const info = await emailAdapter.sendEmail(user.accountData.email, "Email confirmation", textMessage)
+        
+    },
+
+    async sendPasswordRecoveryMessage(user: UserDBModel) {
+        const textMessage = `<h1>Resending email confirmation</h1>
+        <p>To finish registration please follow the link below:
+            <a href='${APP_URL}/confirm-email?code=${user.emailConfirmation.confirmationCode}'>complete registration</a>
+        </p>`
+        
+        const info = await emailAdapter.sendEmail(user.accountData.email, "Resending email confirmation", textMessage)
+        
+    }
+}
