01a1e0b26414c6378953cb98307bc92433c68a59
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-05-10 06:46:59 +0300

    added JWT_SECRET_REFRESH_TOKEN and JWT_SECRET_ACCESS_TOKEN

--- src/application/jwt-service.ts
+++ src/application/jwt-service.ts
@@ -1,21 +1,21 @@
 import jwt from 'jsonwebtoken'
 import { UserDBModel } from "../models/users/UserDBModel";
-import { ObjectId } from 'mongodb';
 import { settings } from '../settings';
 
-const JWT_SECRET = settings.JWT_SECRET
+const JWT_SECRET_ACCESS_TOKEN = settings.JWT_SECRET_ACCESS_TOKEN
+const JWT_SECRET_REFRESH_TOKEN = settings.JWT_SECRET_REFRESH_TOKEN
 
 export const jwtService = {
 
     async createJWT(user:UserDBModel): Promise<string> {
-        const token = jwt.sign({ UserId: user._id }, JWT_SECRET, { expiresIn: '1h' })
+        const token = jwt.sign({ UserId: user._id }, JWT_SECRET_ACCESS_TOKEN, { expiresIn: '1h' })
         return token 
     },
 
     async getUserIdByToken(token: string): Promise<string | undefined> {
         try {
             
-            const result:any = jwt.verify(token, JWT_SECRET)
+            const result:any = jwt.verify(token, JWT_SECRET_ACCESS_TOKEN)
             return result.UserId
             
         } catch (error) {
