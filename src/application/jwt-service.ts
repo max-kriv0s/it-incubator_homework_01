dae037dd73515e5b6df8b7a0a5b01bd5ec7e6c4c
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-05-13 02:26:09 +0300

    Added work with access and refresh tokens

--- src/application/jwt-service.ts
+++ src/application/jwt-service.ts
@@ -5,21 +5,37 @@ import { settings } from '../settings';
 const JWT_SECRET_ACCESS_TOKEN = settings.JWT_SECRET_ACCESS_TOKEN
 const JWT_SECRET_REFRESH_TOKEN = settings.JWT_SECRET_REFRESH_TOKEN
 
+const JWT_ACCESS_TOKEN_EXPIRES_IN = settings.JWT_ACCESS_TOKEN_EXPIRES_IN
+const JWT_REFRESH_TOKEN_EXPIRES_IN = settings.JWT_REFRESH_TOKEN_EXPIRES_IN
+
 export const jwtService = {
 
-    async createJWT(user:UserDBModel): Promise<string> {
-        const token = jwt.sign({ UserId: user._id }, JWT_SECRET_ACCESS_TOKEN, { expiresIn: '1h' })
+    async createJWTAccessToken(user:UserDBModel): Promise<string> {
+        const token = jwt.sign({ UserId: user._id }, JWT_SECRET_ACCESS_TOKEN, { expiresIn: JWT_ACCESS_TOKEN_EXPIRES_IN })
         return token 
     },
 
-    async getUserIdByToken(token: string): Promise<string | undefined> {
+    async createJWTRefreshToken(user:UserDBModel): Promise<string> {
+        const refreshToken = jwt.sign({ UserId: user._id }, JWT_SECRET_REFRESH_TOKEN, { expiresIn: JWT_REFRESH_TOKEN_EXPIRES_IN })
+        return refreshToken 
+    },
+
+    async findUserIdByAccessToken(token: string): Promise<string | null> {
+        return await this.findUserIDByToken(token, JWT_SECRET_ACCESS_TOKEN)
+    },
+
+    async  findUserIDByRefreshToken(token: string): Promise<string | null> {
+        return await this.findUserIDByToken(token, JWT_SECRET_REFRESH_TOKEN)
+    },
+
+    async findUserIDByToken(token: string, jwt_secret: string): Promise<string | null> {
         try {
             
-            const result:any = jwt.verify(token, JWT_SECRET_ACCESS_TOKEN)
+            const result:any = jwt.verify(token, jwt_secret)
             return result.UserId
             
         } catch (error) {
-            return undefined
+            return null
         }
     }
 }
