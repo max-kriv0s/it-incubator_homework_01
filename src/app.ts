8df4107e1af8c79038d8138efc7b7d2afae4381c
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-05-15 16:03:55 +0300

    added SecurityDevicesRouter

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from 'dotenv'
 dotenv.config() 
 
+import cookieParser from "cookie-parser";
 import { routerVideos } from "./routes/videos-router";
 import { routerTesting } from "./routes/testing-router";
 import { routerBlogs } from "./routes/blogs-router";
@@ -9,7 +10,7 @@ import { routerPosts } from "./routes/posts-router";
 import { routerUsers } from "./routes/users-router";
 import { routerAuth } from "./routes/auth-router";
 import { commentsRouter } from "./routes/comments-router";
-import cookieParser from "cookie-parser";
+import { SecurityDevicesRouter } from "./routes/securityDevices-router";
 
 
 export const app = express()
@@ -24,3 +25,4 @@ app.use('/posts', routerPosts)
 app.use('/users', routerUsers)
 app.use('/auth', routerAuth)
 app.use('/comments', commentsRouter)
+app.use('/security/devices', SecurityDevicesRouter)
