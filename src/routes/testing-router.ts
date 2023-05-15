c09cd1c1657786b144de4166bd6071b51e641d54
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-05-15 15:33:49 +0300

    fix and added deleteDevices

--- src/routes/testing-router.ts
+++ src/routes/testing-router.ts
@@ -4,16 +4,18 @@ import { blogsRepository } from "../repositories/blogs-repository";
 import { postsRepository } from "../repositories/posts-repository";
 import { usersRepository } from "../repositories/users-repository";
 import { commentsRepository } from "../repositories/comments-repository";
+import { devicesRepository } from "../repositories/devices-repository";
 
 
 export const routerTesting = Router()
 
 routerTesting.delete('/all-data', async (req: Request, res: Response) => {
-    videoRepository.deleteVideos()
-    blogsRepository.deleteBlogs()
-    postsRepository.deletePosts()
-    usersRepository.deleteUsers()
-    commentsRepository.deleteComments()
+    await videoRepository.deleteVideos()
+    await blogsRepository.deleteBlogs()
+    await postsRepository.deletePosts()
+    await usersRepository.deleteUsers()
+    await commentsRepository.deleteComments()
+    await devicesRepository.deleteDevices()
 
     res.send(204)
 })
