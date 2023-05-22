10c2b382871d73863bb2315ecb5fb759fe8ae129
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-05-22 18:15:22 +0300

    fix

--- src/application/jwt-service.ts
+++ src/application/jwt-service.ts
@@ -2,12 +2,22 @@ import jwt from 'jsonwebtoken'
 import { UserDBModel } from "../models/users/UserDBModel";
 import { settings } from '../settings';
 import { ObjectId } from 'mongodb';
+import { DecodeTokenModel } from '../models/token/DecodeTokenModel';
+import { validID } from '../repositories/db';
+import { DataTokenModel } from '../models/token/DataTokenModel';
 
 const JWT_SECRET_ACCESS_TOKEN = settings.JWT_SECRET_ACCESS_TOKEN
 const JWT_SECRET_REFRESH_TOKEN = settings.JWT_SECRET_REFRESH_TOKEN
 
 const JWT_ACCESS_TOKEN_EXPIRES_IN = settings.JWT_ACCESS_TOKEN_EXPIRES_IN
 const JWT_REFRESH_TOKEN_EXPIRES_IN = settings.JWT_REFRESH_TOKEN_EXPIRES_IN
+
+interface DataToken {
+    UserId: string;
+    deviceId: string;
+    iat: number;
+    exp: number;
+}
 
 export const jwtService = {
 
@@ -21,27 +31,54 @@ export const jwtService = {
         return refreshToken 
     },
 
-    async findUserIdByAccessToken(token: string): Promise<string | null> {
-        return await this.findUserIDByToken(token, JWT_SECRET_ACCESS_TOKEN)
+    async ReadAndCheckTokenAccessToken(token: string): Promise<DataTokenModel | null> {
+        return this.ReadAndCheckToken(token, JWT_SECRET_ACCESS_TOKEN)
     },
 
-    async  findUserIDByRefreshToken(token: string): Promise<string | null> {
-        return await this.findUserIDByToken(token, JWT_SECRET_REFRESH_TOKEN)
+    async ReadAndCheckTokenRefreshToken(token: string): Promise<DataTokenModel | null> {
+        return this.ReadAndCheckToken(token, JWT_SECRET_REFRESH_TOKEN)
     },
 
-    async findUserIDByToken(token: string, jwt_secret: string): Promise<string | null> {
+    async ReadAndCheckToken(token: string, jwt_secret: string): Promise<DataTokenModel | null> {
+        
+        
         try {
-            
-            const result:any = jwt.verify(token, jwt_secret)
-            return result.UserId
+            const result = jwt.verify(token, jwt_secret) as DataToken
+
+            return {
+                userId: result.UserId,
+                deviceId: result.deviceId,
+                issuedAd: new Date(result.iat * 1000).toISOString(),
+                expirationTime: new Date(result.exp * 1000).toISOString()
+            }
             
         } catch (error) {
             return null
         }
     },
 
-    async decodeToken(token: string) {
-        const result = jwt.decode(token, { complete: true })
-        return result
+    // async decodeRefreshToken(token: string): Promise<DecodeTokenModel | null> {
+        
+    //     try {
+    //         const result = jwt.decode(token, { json: true })
+    //         if (!result) return null
+    //         if (!result.UserId || !result.deviceId || !result.iat || !result.exp) return null
+            
+    //         if (!validID(result.UserId) || !validID(result.deviceId)) return null
+    
+    //         return {
+    //             userId: new ObjectId(result.UserId),
+    //             deviceId: new ObjectId(result.deviceId),
+    //             lastActiveDate: new Date(result.iat * 1000).toISOString(),
+    //             expirationTime: new Date(result.exp * 1000).toISOString()
+    //         }           
+    //     } catch (error) {
+    //         return null
+    //     }
+
+    // },
+
+    async validDebidRefreshToken(debidRefreshToken: DataTokenModel, deviceId: ObjectId, userId: ObjectId): Promise <boolean> {
+        return debidRefreshToken.deviceId === deviceId.toString() && debidRefreshToken.userId === userId.toString()
     }
 }
