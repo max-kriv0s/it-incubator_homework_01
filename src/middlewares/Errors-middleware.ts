ab11690f57f2bf02a96c34bc5e801b07dd728c95
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-05-22 23:04:33 +0300

    fix

--- src/middlewares/Errors-middleware.ts
+++ src/middlewares/Errors-middleware.ts
@@ -1,5 +1,5 @@
 import {Response, Request, NextFunction} from "express"
-import { validationResult } from "express-validator";
+import { ValidationError, validationResult } from "express-validator";
 import { StatusCodes } from "http-status-codes";
 import { APIErrorResult, FieldError } from "../types/APIErrorModels";
 
@@ -8,7 +8,7 @@ export function ErrorsValidate(req: Request, res: Response<APIErrorResult>, next
     const result = validationResult(req);
     if (!result.isEmpty()) {
 
-      const errors = result.array({ onlyFirstError: true })
+      const errors: ValidationError[] = result.array({ onlyFirstError: true })
     //   const messages: FieldError[] = errors.map(err => ({message: err.msg, field: err.param}))
     
     //   return res.status(StatusCodes.BAD_REQUEST).json({ "errorsMessages": messages });
