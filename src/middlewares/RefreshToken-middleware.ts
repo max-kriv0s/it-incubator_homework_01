9dd19ae2e0f70002ac5bca7e4c7630124170a7d9
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-05-22 23:04:42 +0300

    fix

--- src/middlewares/RefreshToken-middleware.ts
+++ src/middlewares/RefreshToken-middleware.ts
@@ -1,20 +1,24 @@
 import { Request, Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
 import { jwtService } from "../application/jwt-service";
-import { usersService } from "../domain/users-service";
+import { securityDevicesService } from "../domain/security-devices-service";
 
 export const RefreshTokenMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     
     const refreshToken = req.cookies.refreshToken
     if (!refreshToken) return res.sendStatus(StatusCodes.UNAUTHORIZED)
 
-    const userId = await jwtService.findUserIDByRefreshToken(refreshToken)
-    if (!userId) return res.sendStatus(StatusCodes.UNAUTHORIZED)
+    const dataRefreshToken = await jwtService.ReadAndCheckTokenRefreshToken(refreshToken)
+    if (!dataRefreshToken || !dataRefreshToken.userId || !dataRefreshToken.deviceId) {
+        return res.sendStatus(StatusCodes.UNAUTHORIZED)
+    }
 
-    const user = await usersService.findUserById(userId)
-    if (!user || user.refreshToken !== refreshToken) return res.sendStatus(StatusCodes.UNAUTHORIZED)
+    const isVerify = await securityDevicesService.verifySecurityDeviceByToken(dataRefreshToken)
+    if (!isVerify) return res.sendStatus(StatusCodes.UNAUTHORIZED)
 
-    req.userId = userId
+    req.userId = dataRefreshToken.userId
+    req.deviceId = dataRefreshToken.deviceId
+
     next()
     
 }
