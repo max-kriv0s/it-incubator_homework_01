0a89cfc42f91555e1bab2cbb4e39f48700a30443
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-05-28 00:37:36 +0300

    deleted videos

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,6 @@ import dotenv from 'dotenv'
 dotenv.config() 
 
 import cookieParser from "cookie-parser";
-import { routerVideos } from "./routes/videos-router";
 import { routerTesting } from "./routes/testing-router";
 import { routerBlogs } from "./routes/blogs-router";
 import { routerPosts } from "./routes/posts-router";
@@ -20,7 +19,6 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use('/testing', routerTesting)
-app.use('/videos', routerVideos)
 app.use('/blogs', routerBlogs)
 app.use('/posts', routerPosts)
 app.use('/users', routerUsers)
