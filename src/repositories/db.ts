b0aaa0b75a1a75ce243ecca3e22a9086514083fa
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-05-28 00:36:48 +0300

    mangoose refactoring

--- src/repositories/db.ts
+++ src/repositories/db.ts
@@ -1,55 +1,17 @@
 import mongoose from 'mongoose'
-import { MongoClient, ObjectId } from "mongodb"
-import { VideoViewModel } from "../models/videos/VideoViewModel"
-import { BlogDbModel } from "../models/blogs/BlogDbModel"
-import { PostDbModel } from "../models/posts/PostDbModel"
-import { UserDBModel } from "../models/users/UserDBModel"
 import { settings } from "../settings"
-import { CommentDBModel } from "../models/comments/CommentDBModel"
-import { SecurityDevicesDBModel } from "../models/security-devices/SecurityDevicesDBModel"
-import { APICallsModel } from "../models/APICallsModel"
+import { ObjectId } from 'mongodb'
 
 
 const MONGO_URI = settings.MONGO_URI
 const DB_NAME = settings.DB_NAME
 
-// videos
-
-// blogs
-export const BlogSchema = new mongoose.Schema<BlogDbModel>({
-    name: {type: String, required: true},
-    description: {type: String, required: true},
-    websiteUrl: {type: String, required: true},
-    createdAt: {type: String, required: true},
-    isMembership: {type: Boolean, default: false}
-})
-
-export const BlogModel = mongoose.model<BlogDbModel>('blogs', BlogSchema)
-
-
-
-
-export const client = new MongoClient(MONGO_URI)
-const db = client.db()
-
-export const videosCollection = db.collection<VideoViewModel>('videos')
-// export const blogsCollection = db.collection<BlogDbModel>('blogs')
-export const postsCollection = db.collection<PostDbModel>('posts')
-export const usersCollection = db.collection<UserDBModel>('users')
-export const commentsCollection = db.collection<CommentDBModel>('comments')
-export const securityDevicesCollection = db.collection<SecurityDevicesDBModel>('securityDevices')
-export const apiCallsCollection = db.collection<APICallsModel>('apiCalls') 
-
 export const runDB = async () => {
     try {
         await mongoose.connect(MONGO_URI, {dbName: DB_NAME})
-
-        await client.connect()
-        await db.command({ ping: 1 });
         console.log('Connect to successfully to server')
     } catch (e) {
         console.log('Don\'t connected successfully to server')
-        await client.close()
         await mongoose.disconnect()
     }
 }
