2b25d18b45bfe1086a15e2aadfd1a0954d581b7b
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-05-27 22:04:08 +0300

    fix

--- src/models/posts/PostDbModel.ts
+++ src/models/posts/PostDbModel.ts
@@ -1,11 +1,10 @@
-import { ObjectId } from "mongodb"
+import { ObjectId, WithId } from "mongodb"
 
-export type PostDbModel = {
-    _id: ObjectId
+export type PostDbModel = WithId<{
     title: string
     shortDescription: string
     content: string
     blogId: ObjectId
     blogName: string
     createdAt: string
-}
+}>
