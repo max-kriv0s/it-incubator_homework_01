b4f5c18faac7a9c2ed9c14a44d75d1479812de22
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-05-28 00:34:22 +0300

    deleted file

--- src/types/DataBaseMode.ts
+++
@@ -1,7 +0,0 @@
-import { BlogViewModel } from "../models/blogs/BlogViewModel";
-import { PostViewModel } from "../models/posts/PostViewModel";
-import { VideoViewModel } from "../models/videos/VideoViewModel";
-
-export type DataBaseModel = VideoViewModel[] 
-                            | BlogViewModel[] 
-                            | PostViewModel[]
