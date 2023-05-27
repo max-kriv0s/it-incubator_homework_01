859d3e6b917a5fe3818dd2ab27b99031bbf52aaa
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-05-27 20:45:25 +0300

    fix

--- src/routes/testing-router.ts
+++ src/routes/testing-router.ts
@@ -6,18 +6,34 @@ import { usersRepository } from "../repositories/users-repository";
 import { commentsRepository } from "../repositories/comments-repository";
 import { securityDevicesRepository } from "../repositories/security-devices-repository";
 import { apiCallsRepository } from "../repositories/api-calls-repository";
+import { StatusCodes } from "http-status-codes";
 
 
 export const routerTesting = Router()
 
 routerTesting.delete('/all-data', async (req: Request, res: Response) => {
-    await videoRepository.deleteVideos()
-    await blogsRepository.deleteBlogs()
-    await postsRepository.deletePosts()
-    await usersRepository.deleteUsers()
-    await commentsRepository.deleteComments()
-    await securityDevicesRepository.deleteAllDevicesSessions()
-    await apiCallsRepository.deleteCalls()
+    // await videoRepository.deleteVideos()
+    // await blogsRepository.deleteBlogs()
+    // await postsRepository.deletePosts()
+    // await usersRepository.deleteUsers()
+    // await commentsRepository.deleteComments()
+    // await securityDevicesRepository.deleteAllDevicesSessions()
+    // await apiCallsRepository.deleteCalls()
 
-    res.send(204)
+// можно написать так
+    try {
+        await Promise.all([
+            videoRepository.deleteVideos(),
+            blogsRepository.deleteBlogs(),
+            postsRepository.deletePosts(),
+            usersRepository.deleteUsers(),
+            commentsRepository.deleteComments(),
+            securityDevicesRepository.deleteAllDevicesSessions(),
+            apiCallsRepository.deleteCalls()
+        ])
+        return res.send(StatusCodes.NO_CONTENT)
+    } catch (error) {
+        return res.send(StatusCodes.INTERNAL_SERVER_ERROR)
+    }
+
 })
