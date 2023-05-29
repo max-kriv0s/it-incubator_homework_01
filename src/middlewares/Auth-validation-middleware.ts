cfce1a5373c428fd61ba5068b66b29d2212c4776
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-05-30 00:30:36 +0300

    added AuthNewPasswordRecoveryValidate

--- src/middlewares/Auth-validation-middleware.ts
+++ src/middlewares/Auth-validation-middleware.ts
@@ -7,10 +7,23 @@ export const AuthRegistrationConfirmationCodeValidate = [
         .isString()
 ]
 
-export const AuthRegistrationEmailResendingValodate = [
+export const AuthRegistrationEmailResendingValidate = [
     body('email')
         .trim()
         .exists({ checkFalsy: true })
         .isString()
         .isEmail()
+]
+
+export const AuthNewPasswordRecoveryValidate = [
+    body('newPassword')
+        .trim()
+        .exists({ checkFalsy: true })
+        .isString()
+        .isLength({ min: 6, max: 20 })
+        .withMessage('must be between 6 and 20 characters'),
+    body('recoveryCode')
+        .trim()
+        .exists({ checkFalsy: true })
+        .isString()
 ]
