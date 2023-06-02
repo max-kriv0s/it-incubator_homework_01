6ebb32a577b4aab4f2c74d7a15e46d58fc35aa7d
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-06-02 23:52:02 +0300

    Transfer to class

--- src/domain/users-service.ts
+++ src/domain/users-service.ts
@@ -2,7 +2,6 @@ import bcrypt from "bcrypt"
 import add from 'date-fns/add'
 import {v4 as uuidv4 } from 'uuid'
 import { UserCreateModel } from "../models/users/UserCreateModel";
-import { usersRepository } from "../repositories/users-repository";
 import { PaginatorUserDBModel } from "../types/PaginatorType";
 import { QueryParamsUsersModel } from "../types/QueryParamsModels";
 import { UserDBModel, UserPasswordRecovery } from "../models/users/UserModel";
@@ -13,11 +12,13 @@ import { GetDescriptionOfError } from "../utils/utils";
 import { UpdateTokenModel } from "../models/auth/UpdateTokenModel";
 import { securityDevicesService } from "./security-devices-service";
 import { settings } from "../settings";
+import { UsersRepository } from "../repositories/users-repository";
 
 
 const CODE_LIFE_TIME = settings.CODE_LIFE_TIME
 
-export const usersService = {
+export class UsersService {
+    constructor(protected usersRepository: UsersRepository) {}
 
     async getAllUsers(queryParams: QueryParamsUsersModel): Promise<PaginatorUserDBModel> {
 
@@ -28,7 +29,7 @@ export const usersService = {
         const searchLoginTerm = queryParams.searchLoginTerm ? queryParams.searchLoginTerm : null
         const searchEmailTerm = queryParams.searchEmailTerm ? queryParams.searchEmailTerm : null
 
-        const users = await usersRepository.getAllUsers(
+        const users = await this.usersRepository.getAllUsers(
             searchLoginTerm,
             searchEmailTerm,
             pageNumber,
@@ -38,10 +39,10 @@ export const usersService = {
         )
 
         return users
-    },
+    }
 
     async createUser(body: UserCreateModel): Promise<UserDBModel | null> {
-
+        
         const passwordHash = await this._generatePasswordHash(body.password)
 
         const newUser: UserServiceModel = {
@@ -58,36 +59,30 @@ export const usersService = {
             }
         }
 
-        const createdUser = await usersRepository.createUser(newUser)
-    
-        return createdUser
-    },
+        return this.usersRepository.createUser(newUser)
+    }
 
     async deleteUserById(id: string): Promise<boolean> {
-        return usersRepository.deleteUserById(id)
-    },
+        return this.usersRepository.deleteUserById(id)
+    }
 
-    async _generateHash(password: string, salt: string) {
-
-        const hash = await bcrypt.hash(password, salt)
-        return hash
-    },
+    async _generateHash(password: string, salt: string): Promise<string> {
+        return bcrypt.hash(password, salt)
+    }
 
     async _generatePasswordHash(password: string): Promise<string> {
-        
         const passwordSalt = await bcrypt.genSalt(10)
-        const passwordHash = await this._generateHash(password, passwordSalt)
-        return passwordHash
-    },
+        return this._generateHash(password, passwordSalt)
+    }
 
     async createUserForEmailConfirmation(body: UserCreateModel): Promise<APIErrorResult | null> {
 
-        const userByLogin = await usersRepository.findByLoginOrEmail(body.login)
+        const userByLogin = await this.usersRepository.findByLoginOrEmail(body.login)
         if (userByLogin) {
             return GetDescriptionOfError("user already exists", "login")
         }    
 
-        const userByEmail = await usersRepository.findByLoginOrEmail(body.email)
+        const userByEmail = await this.usersRepository.findByLoginOrEmail(body.email)
         if (userByEmail) {
             return GetDescriptionOfError("user already exists", "email")
         }
@@ -108,26 +103,18 @@ export const usersService = {
             }
         }
         
-        const createdUser = await usersRepository.createUser(newUser)
-        if (!createdUser) return null    
+        const createdUser = await this.usersRepository.createUser(newUser)
+        if (!createdUser) return null
 
-        try {
-            // await 
-            emailManager.sendEmailConfirmationMessage(createdUser.accountData.email, createdUser.emailConfirmation.confirmationCode)
-        } catch (error) {
-            console.error(error)
-
-            // return GetDescriptionOfError("Mail sending error", "email")
-        }
+        emailManager.sendEmailConfirmationMessage(createdUser.accountData.email, createdUser.emailConfirmation.confirmationCode)
 
         return null
-
-    },
+    }
 
     async checkCredentials(loginOrEmail: string, password: string, ip: string, 
             reqUserAgent: string | undefined): Promise<UpdateTokenModel | null> {
 
-        const user = await usersRepository.findByLoginOrEmail(loginOrEmail)
+        const user = await this.usersRepository.findByLoginOrEmail(loginOrEmail)
         if (!user) return null
 
         if (!user.emailConfirmation.isConfirmed) return null
@@ -136,23 +123,23 @@ export const usersService = {
         if (!validPassword) return null
 
         return securityDevicesService.createUserTokens(user, ip, reqUserAgent)
-    },
+    }
 
     async findUserById(userId: string): Promise<UserDBModel | null> {
-        return usersRepository.findUserById(userId)
-    },
+        return this.usersRepository.findUserById(userId)
+    }
 
     async confirmRegistration(code: string): Promise<APIErrorResult | null> {
-        const isUpdated = await usersRepository.confirmRegistration(code)
+        const isUpdated = await this.usersRepository.confirmRegistration(code)
 
         if (!isUpdated) return GetDescriptionOfError("User update error", "code")
 
         return null
-    },
+    }
 
     async resendingConfirmationCodeToUser(email: string): Promise<APIErrorResult | null> {
         
-        const user = await usersRepository.findByLoginOrEmail(email)
+        const user = await this.usersRepository.findByLoginOrEmail(email)
         if (!user) return GetDescriptionOfError("User not found", "email")
 
         if (user.emailConfirmation.isConfirmed) return GetDescriptionOfError("Email confirmed", "email")
@@ -163,29 +150,22 @@ export const usersService = {
             isConfirmed: false
         }
 
-        const isUpdated = await usersRepository.updateDataEmailConfirmation(user, emailConfirmation)
+        const isUpdated = await this.usersRepository.updateDataEmailConfirmation(user, emailConfirmation)
         if (!isUpdated) return GetDescriptionOfError("User update error", "email")
 
-        try {
-            // await 
-            emailManager.sendPasswordRecoveryMessage(user.accountData.email, emailConfirmation.confirmationCode)
-        } catch (error) {
-            console.error(error)
-            // return GetDescriptionOfError("Mail sending error", "email")
-        }
+        emailManager.sendPasswordRecoveryMessage(user.accountData.email, emailConfirmation.confirmationCode)
 
         return null
-        
-    },
+    }
 
     async updateUserRefreshToken(userID: string, deviceId: string, ip: string, 
         reqUserAgent: string | undefined): Promise<UpdateTokenModel | null> {
     
-        const user = await usersRepository.findUserById(userID)
+        const user = await this.usersRepository.findUserById(userID)
         if (!user) return null
 
         return securityDevicesService.updateUserTokens(user, deviceId, ip, reqUserAgent)
-    },
+    }
 
     async passwordRecovery(email: string) {
         
@@ -194,26 +174,21 @@ export const usersService = {
             expirationDate: add(new Date(), CODE_LIFE_TIME),
         }
 
-        const user = await usersRepository.findByLoginOrEmail(email)
+        const user = await this.usersRepository.findByLoginOrEmail(email)
         if (user) {
-            await usersRepository.updatePasswordRecovery(user, passwordRecovery)
+            await this.usersRepository.updatePasswordRecovery(user, passwordRecovery)
         }
 
-        try {
-            emailManager.sendPasswordRecovery(email, passwordRecovery.recoveryCode)
-        } catch (error) {
-            console.error(error)
-        }
-    },
+        emailManager.sendPasswordRecovery(email, passwordRecovery.recoveryCode)
+    }
 
     async newPassword(newPassword: string, recoveryCode: string): Promise<boolean> {
-        const user = await usersRepository.findUserByRecoveryCode(recoveryCode)
+        const user = await this.usersRepository.findUserByRecoveryCode(recoveryCode)
         if (!user) return false
         if (user.passwordRecovery.expirationDate < new Date()) return false
 
         const passwordHash = await this._generatePasswordHash(newPassword)
 
-
-        return usersRepository.updateUserPassword(user, passwordHash)
+        return this.usersRepository.updateUserPassword(user, passwordHash)
     }
 }
