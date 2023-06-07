f0d87cfbac45696427232379e529932670dbf057
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-06-07 14:20:07 +0300

    added MyResult and ResultCode

--- src/types/types.ts
+++ src/types/types.ts
@@ -5,3 +5,15 @@ export type RequestsWithBody<T> = Request<{},{},T>
 export type RequestsWithParamsAndBody<P, B> = Request<P,{},B>
 export type RequestsWithParamsAndQuery<P, Q> = Request<P, {}, {}, Q>
 export type RequestsQuery<Q> = Request<{}, {}, {}, Q>
+
+export enum ResultCode {
+    success,
+    notFound,
+    ServerError
+}
+
+export type MyResult<T> = {
+    data: T | null
+    code: ResultCode
+    errorMessage: string | null
+}
