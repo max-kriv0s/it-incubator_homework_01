4265a6c52576afea9ea38488324f937dcbae0135
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-06-07 14:22:25 +0300

    added likeStatusByCommentID

--- src/routes/comments-router/comments-router.ts
+++ src/routes/comments-router/comments-router.ts
@@ -1,16 +1,20 @@
 import { Router } from "express";
 import { BearerAuthMiddleware } from "../../middlewares/BearerAuth-middleware";
-import { CommentUserIDMiddleware, CommentValidate } from "../../middlewares/Comment-validate-middleware";
+import { CommentUserIDMiddleware, CommentValidate, CommentsBearerMiddleware } from "../../middlewares/Comment-validate-middleware";
 import { ErrorsValidate } from "../../middlewares/Errors-middleware";
 import { commetsController } from "../../composition-root";
+import { LikeValidate } from "../../middlewares/Like-validate";
 
 
 export const commentsRouter = Router({})
 
-commentsRouter.get('/:id', commetsController.findCommentByID.bind(commetsController))
+commentsRouter.get('/:id', CommentsBearerMiddleware, commetsController.findCommentByID.bind(commetsController))
 commentsRouter.put('/:commentId', BearerAuthMiddleware, CommentValidate, ErrorsValidate, CommentUserIDMiddleware,
     commetsController.updatedComment.bind(commetsController)
 )
 commentsRouter.delete('/:commentId', BearerAuthMiddleware, CommentUserIDMiddleware,
     commetsController.deleteCommentByID.bind(commetsController)
+)
+commentsRouter.put('/:commentId/like-status', BearerAuthMiddleware, LikeValidate, ErrorsValidate,
+    commetsController.likeStatusByCommentID.bind(commetsController)
 )
