bafcbeb313fbef76993ef1cb0d75b0979ddafbb0
Maxim Krivoshein <max.kriv0s@gmail.com>
2023-06-07 14:23:17 +0300

    added likes-repository.ts

---
+++ src/repositories/likes/likes-repository.ts
@@ -0,0 +1,43 @@
+import { LikeClass, LikeModel, LikeStatus } from "../../models/likes/LikeModel";
+import { getMyResult } from "../../utils/utils";
+import { MyResult, ResultCode } from "../../types/types";
+import { ObjectId } from "mongodb";
+import { validID } from "../db";
+
+export class LikeRepository {
+
+    async findLike(commentId: string, userId: string): Promise<MyResult<LikeClass>> {
+        if (!validID(commentId) || !validID(userId)) return getMyResult<LikeClass>(ResultCode.notFound)
+        
+        const like = await LikeModel.findOne({commentId, userId}).exec()
+        if (!like) return getMyResult<LikeClass>(ResultCode.notFound)
+
+        return getMyResult<LikeClass>(ResultCode.success, like)
+    }
+
+    async createLike(commentId: string, userId: string, status: LikeStatus): Promise<MyResult<LikeClass>> {
+        if (!validID(commentId) || !validID(userId)) return getMyResult<LikeClass>(ResultCode.notFound)
+        
+        const newLike = await LikeModel.create({
+            commentId,
+            userId,
+            status
+        })
+
+        return getMyResult<LikeClass>(ResultCode.success, newLike)
+    }
+
+    async updateLike(id: ObjectId, status: string): Promise<MyResult<LikeClass>> {
+        const result = await LikeModel.updateOne({_id: id}, {status})
+        if (result.matchedCount !== 1) return getMyResult(ResultCode.ServerError)
+
+        return getMyResult<LikeClass>(ResultCode.success)
+    }
+
+    async deleteLike(id: ObjectId): Promise<MyResult<LikeClass>> {
+        const result = await LikeModel.deleteOne({_id: id})
+        if (result.deletedCount !== 1) return getMyResult<LikeClass>(ResultCode.notFound)
+
+        return getMyResult<LikeClass>(ResultCode.success)
+    }
+}
